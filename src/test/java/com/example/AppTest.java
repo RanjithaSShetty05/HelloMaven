cda51c8158a60836354eb8e8d52911a599565bf4
Ranjitha S Shetty <ranjithashettys_csd@ksit.edu.in>
2026-05-05 19:53:42 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example;
 
 import junit.framework.Test;
@@ -37,43 +37,5 @@ public class AppTest
         assertTrue( true );
     }
 }
-=======
-package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
->>>>>>> 9a742db9d98dbe751ed349a34d89183d1d069717
+  
